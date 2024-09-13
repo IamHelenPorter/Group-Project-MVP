0b9836cfeb54caeecaca9a048500888bf8282d99
Helen-Porter <iamhelenporter@gmail.com>
2024-09-13 03:26:05 -0500

    Initial Express commit

---
+++ model/init_db.sql
@@ -0,0 +1,32 @@
+--
+-- Drop Tables
+--
+
+SET foreign_key_checks = 0;
+DROP TABLE if exists goals;
+DROP TABLE if exists rewards;
+SET foreign_key_checks = 1;
+
+--
+-- Create Tables
+--
+
+CREATE TABLE `goals`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `goal` VARCHAR(255) NOT NULL,
+    `difflevel` INT NOT NULL,
+    `dayofweek` VARCHAR(255) NOT NULL,
+    `completed` BOOLEAN NOT NULL
+);
+
+CREATE TABLE `rewards`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `reward` VARCHAR(255) NOT NULL,
+    `gemlevel` VARCHAR(255) NOT NULL
+);
+
+
+
+
+
+
