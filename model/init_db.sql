539e8d995a768bc4899f2f293bf4112983052d3c
Helen-Porter <iamhelenporter@gmail.com>
2024-09-15 17:15:20 -0500

    get, post, delete routes for doctor table created, still needs attention

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,27 +3,74 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists goals;
-DROP TABLE if exists rewards;
+DROP TABLE if exists appointments;
+DROP TABLE if exists hospital;
+DROP TABLE if exists user;
+DROP TABLE if exists doctor;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE `goals`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `goal` VARCHAR(255) NOT NULL,
-    `difflevel` INT NOT NULL,
-    `dayofweek` VARCHAR(255) NOT NULL,
-    `completed` BOOLEAN NOT NULL
+CREATE TABLE `appointments`(
+    `appointment_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NULL,
+    `doctor_id` INT NOT NULL,
+    `start_time` DATETIME NOT NULL,
+    `status` ENUM(
+        'booked',
+        'cancelled',
+        'completed'
+    ) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL
 );
+CREATE TABLE `hospital`(
+    `hospital_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `emergency` BOOLEAN NULL,
+    `departments` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `user`(
+    `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `role` ENUM('patient', 'doctor', 'admin') NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    `date_of_birth` DATE NOT NULL,
+    `image` VARCHAR(255) NULL
+);
+CREATE TABLE `doctor`(
+    `doctor_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `speciality` VARCHAR(255) NOT NULL,
+    `hospital_id` INT NOT NULL,
+    `qualifications` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `doctor` ADD CONSTRAINT `doctor_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`);
 
-CREATE TABLE `rewards`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `reward` VARCHAR(255) NOT NULL,
-    `gemlevel` VARCHAR(255) NOT NULL
-);
+ALTER TABLE
+    `appointments` ADD CONSTRAINT `appointments_doctor_id_foreign` FOREIGN KEY(`doctor_id`) REFERENCES `doctor`(`doctor_id`);
+ALTER TABLE
+    `appointments` ADD CONSTRAINT `appointments_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`user_id`);
+ALTER TABLE
+    `doctor` ADD CONSTRAINT `doctor_hospital_id_foreign` FOREIGN KEY(`hospital_id`) REFERENCES `hospital`(`hospital_id`);
+
+
+
+
+
+
+
+
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
 });
+
+router.get('/doctor', async (req, res) => {
+  try {
+    let results = await db(`SELECT * FROM doctor;`);
+    res.send(results.data)
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+})
+
+// router.get('/doctor/:id', async (req, res) => {
+//   const id = req.params.id; 
+//   try {
+//     const results = await db(`SELECT * FROM doctor WHERE doctor_id = ${id}`);
+//     // NEED TO COMBINE RESULTS TO SEND USER FIRST_NAME AND LAST_NAME AS WELL AS DOCTOR INFO
+//   } catch (err) {
+//     res.status(500).send({error: err.message});
+//   }
+// })
+
+router.post('/doctor', async (req, res) => {
+  const { user_id, speciality, hospital_id, qualifications } = req.body;
+  const insertDoctor = `INSERT INTO doctor (user_id, speciality, hospital_id, qualifications)
+  VALUES ('${user_id}', '${speciality}', '${hospital_id}', '${qualifications}');`;
+  try {
+    await db(insertDoctor);
+    const doctorResults = await db(`SELECT * FROM doctor WHERE user_id = ${user_id};`);
+    // const userResults = await db(`SELECT * FROM user WHERE user_id = ${user_id};`);
+    // const combinedResults = {...doctorResults, userResults.data.first_name, userResults.data.last_name };
+    // res.send(combinedResults)
+       // NEED TO COMBINE RESULTS TO SEND USER FIRST_NAME AND LAST_NAME AS WELL AS DOCTOR INFO
+    res.send(doctorResults)
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+})
+
+// HOW CAN I CHECK THAT DOCTOR_ID EXISTS
+router.delete('/doctor/:id', async (req, res) => {
+  const { id } = req.params;
+  const doctorId = Number(id)
+  try {
+    await db(`DELETE FROM doctor WHERE doctor_id = ${doctorId};`);
+    const results = await db(`SELECT * FROM doctor;`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+})
+
+router.get('/appointments', async (req, res) => {
+  try {
+    let results = await db(`SELECT * FROM appointments`);
+    res.send(results.data)
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+})
 
 module.exports = router;
