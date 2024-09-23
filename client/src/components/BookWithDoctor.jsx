fe207b6411d3e48bcf1355bb4bb941cb9d93dd7c
Helen-Porter <iamhelenporter@gmail.com>
2024-09-23 16:50:45 -0500

    rendering of doctor info in BookWithDoctor component functioning

--- client/src/components/BookWithDoctor.jsx
+++ client/src/components/BookWithDoctor.jsx
@@ -8,13 +8,13 @@ export default function BookWithDoctor() {
     const { doctor_id } = useParams();
     
 
-    const [doctor, setDoctor] = useState({})
+    const [doctor, setDoctor] = useState(null)
 
         
     const fetchDoctor = () => {
         axios.get(`http://localhost:4000/doctor/${doctor_id}`)
         .then(response => {
-          setDoctor(response);
+          setDoctor(response.data);
           console.log(response.data)
         })
      };
@@ -30,6 +30,18 @@ export default function BookWithDoctor() {
   return (
     <div>
         <h1>Book With Doctor</h1>
+        {doctor &&
+        <div>
+        <img src={doctor[0].image} alt="Image of Doctor" /> 
+        <h2>Dr. {doctor[0].first_name} {doctor[0].last_name} </h2>
+        <h3>{doctor[0].qualifications}</h3>
+        <h3>{doctor[0].speciality}</h3>
+        <h3>{doctor[0].name}</h3>
+        <h4>{doctor[0].address}</h4>
+        </div>
+        }
+
+
     </div>
   )
 }
