7a0a7642d86a267a23dce0b6f358bef37ae9317b
Helen-Porter <iamhelenporter@gmail.com>
2024-09-27 10:31:13 -0500

    get all available time slots, filtering out already booked appts completed

--- client/src/components/BookWithDoctor.jsx
+++ client/src/components/BookWithDoctor.jsx
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
+import { list } from '@chakra-ui/react';
 
 export default function BookWithDoctor() {
 
+    const now = DateTime.now()
    
     const { doctor_id } = useParams();
     
-    const [selectedDate, setSelectedDate] = useState(null);
-    const [doctor, setDoctor] = useState(null)
-    const [apptsBookedWithDoctor, setApptsBookedWithDoctor] = useState(null)
+    const [selectedDate, setSelectedDate] = useState(now);
+    const [doctor, setDoctor] = useState(null);
+    const [apptsBookedWithDoctor, setApptsBookedWithDoctor] = useState([]);
+    const [listOfAvailableTimes, setListOfAvailableTimes] = useState([]);
+     
 
-        console.log(selectedDate)
+
+    
+        useEffect(() => {
+            fetchDoctor();
+        }, []);
+    
+        useEffect(() => {
+            fetchApptsBookedWithDoctor();
+        }, []);
+    
+        // useEffect(() => {
+        //     getListOfAvailableTimes();
+        // }, []);
         
     const fetchDoctor = () => {
         axios.get(`/api/doctor/${doctor_id}`)
@@ -25,25 +41,46 @@ export default function BookWithDoctor() {
      const fetchApptsBookedWithDoctor = () => {
         axios.get(`/api/appointments/doctor/${doctor_id}`)
         .then(response => {
-            setApptsBookedWithDoctor(response.data);
-            console.log(response.data)
+            setApptsBookedWithDoctor(response.data)
         })
-     }
-
+     };
 
      useEffect(() => {
-        fetchDoctor();
-    }, []);
+        // const getListOfAvailableTimes = (selectedDate) FINISH THIS
 
-    useEffect(() => {
-        fetchApptsBookedWithDoctor();
-    }, []);
+        //Get times that are already booked for selected day
+        if (apptsBookedWithDoctor.length > 0) {
+            const listofUnavailableTimes = [];
+            const parsedSelectedDate = DateTime.fromISO(selectedDate);
+            for ( let i = 0; i < apptsBookedWithDoctor.length; i++ ) {
+    
+                let checkBookedAppt = apptsBookedWithDoctor[i].start_time;
+
+                const parsedCheckBookedAppt = DateTime.fromISO(checkBookedAppt);
+            
+                if (parsedCheckBookedAppt.hasSame(parsedSelectedDate, 'day')) {
+                    listofUnavailableTimes.push(parsedCheckBookedAppt)
+                }
+            }
+
+            const dateAt9AM = parsedSelectedDate.set({ hour: 9, minute: 0, second: 0 });
+            const dateAt5PM = parsedSelectedDate.set({ hour: 17, minute: 0, second: 0});
+            const dur = Duration.fromObject({ hours: 0, minutes: 30 });
+            let incrementedTime = dateAt9AM;
+            let allTimeSlots = [];
+       //Find list of all times, push to array
+            while (incrementedTime < dateAt5PM) {
+                allTimeSlots.push(incrementedTime)
+                incrementedTime = incrementedTime.plus(dur)
+            }
+       // then filter out list of unavailable times
+            let availableTimes = allTimeSlots.filter((e) => listofUnavailableTimes.indexOf(e) < 0)
+            setListOfAvailableTimes(availableTimes);
+       
+        }
+    }, [apptsBookedWithDoctor, selectedDate]); 
 
 
-    const getListOfAvailableTimes = (selectedDate) => {
-        const listofUnavailableTimes = [];
-        //GET ALL APPOINTMENTS BY DOCTOR_ID
-    }
 
 
 
